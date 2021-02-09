2780ce110087697619981d65d2c63a82554b69ca
Ni <huiqingn@3c22fbb6f1a8.ant.amazon.com>
2021-02-10 07:23:09 +0800

    piaozone

--- cicd/lambda_function.py
+++ cicd/lambda_function.py
@@ -3,6 +3,6 @@ import json
 def lambda_handler(event, context):
     # TODO implement
     return {
-        'statusCode': 200,
+        'statusCode': 208,
         'body': json.dumps('Hello from Lambda!')
     }
